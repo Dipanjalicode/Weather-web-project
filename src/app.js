eebb3b7a28d24b2ea437e346e3968a2f7be8dec0
Dipanjalicode <dipanjali9330@gmail.com>
2021-02-01 16:22:36 +0530

    express weather project

---
+++ src/app.js
@@ -0,0 +1,58 @@
+const express=require("express");
+const path=require("path");
+const app=express();
+const hbs=require("hbs");
+const add=path.join(__dirname,"../public");
+const hbspath=path.join(__dirname,"../templets/views");
+const partials_path=path.join(__dirname,"../templets/partials")
+const port=process.env.PORT||8000;
+app.use(express.static(add));
+app.set('view engine', 'hbs');
+app.set('views', hbspath);
+hbs.registerPartials(partials_path);
+
+  
+app.get("",(req,res)=>{
+
+  res.render('index');
+  
+  
+});
+app.get("/about",(req,res)=>{
+  
+  res.render('about');
+  
+  
+});
+app.get("/weather",(req,res)=>{
+  
+  res.render('weather');
+  
+  
+});
+app.get("/me",(req,res)=>{
+  
+  res.render('me');
+});
+  
+app.get("*",(req,res)=>{
+  
+  res.render('error',{
+    error_msg:"opps page not found", 
+    
+  });
+});
+  
+  
+
+
+
+app.listen(port,"localhost",(err)=>{
+  if(err){
+    throw err;
+  }
+  else{
+    console.log("server is running on 8000");
+  }
+})
+
